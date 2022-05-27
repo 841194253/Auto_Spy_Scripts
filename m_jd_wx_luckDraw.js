c07108024ea025ff1dfa5cf2ad6c4f4018e1e9d0
caitou575 <sk18661007200@gmail.com>
2022-05-27 12:59:30 +0800

    number 50

--- m_jd_wx_luckDraw.js
+++ m_jd_wx_luckDraw.js
@@ -222,5 +222,5 @@ $.after = async function () {
     $.msg.push(message)
     $.msg.push($.activityUrl);
 }
-$.run({ whitelist: ['1-5'], wait: [3000, 5000] }).catch(
+$.run({ whitelist: ['1-50'], wait: [3000, 5000] }).catch(
     reason => $.log(reason));
